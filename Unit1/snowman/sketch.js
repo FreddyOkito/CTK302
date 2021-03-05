97c95e6bcb08c4f917c5d356aabc774387b92018
FreddyOkito <70274166+FreddyOkito@users.noreply.github.com>
2021-03-05 11:43:35 -0600

    1

---
+++ Unit1/snowman/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  //background
+  ellipse(width/2, height/2 +25, 50,50);
+  ellipse(width/2, height/2 +50, 100,100);
+  ellipse(width/2, height/2 +75, 150,150);
+
+
+
+}
