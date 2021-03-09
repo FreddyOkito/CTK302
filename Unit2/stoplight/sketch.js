420b3aeb429485a80464603893a7ec692ae1fd49
FreddyOkito <70274166+FreddyOkito@users.noreply.github.com>
2021-03-09 15:11:03 -0600

    .

--- Unit2/stoplight/sketch.js
+++ Unit2/stoplight/sketch.js
@@ -19,42 +19,45 @@ function draw() {
   if (x > width) {
     x = 0;
   }
-  switch (state);
+  switch (state) {
 
-  case 0: // red
-    text("0", 100, 100);
-  fill('red');
-  ellipse(width / 2, height / 2 - 170, 150, 150);
-  fill('grey');
-  ellipse(width / 2, height / 2, 150, 150);
-  ellipse(width / 2, height / 2 + 170, 150, 150);
-  break;
 
-  case 1: // green
-    text("1 ", 100, 100);
-  fill('grey');
-  ellipse(width / 2, height / 2 - 170, 150, 150);
-  ellipse(width / 2, height / 2, 150, 150);
-  fill('green');
-  ellipse(width / 2, height / 2 + 170, 150, 150);
-  break;
 
-  case 2: // yellow
-    text("2", 100, 100);
-  fill('grey');
-  ellipse(width / 2, height / 2 - 170, 150, 150);
-  fill('yellow');
-  ellipse(width / 2, height / 2, 150, 150);
-  fill('grey');
-  ellipse(width / 2, height / 2 + 170, 150, 150);
-  break;
+    case 0: // red
+      text("0", 100, 100);
+      fill('red');
+      ellipse(width / 2, height / 2 - 170, 150, 150);
+      fill('grey');
+      ellipse(width / 2, height / 2, 150, 150);
+      ellipse(width / 2, height / 2 + 170, 150, 150);
+      break;
 
-}
+    case 1: // green
+      text("1 ", 100, 100);
+      fill('grey');
+      ellipse(width / 2, height / 2 - 170, 150, 150);
+      ellipse(width / 2, height / 2, 150, 150);
+      fill('green');
+      ellipse(width / 2, height / 2 + 170, 150, 150);
+      break;
 
-timer = timer + 1;
-if(timer > 100) {
-  timer = 0;
-  if (state > 2) state = 0;
+    case 2: // yellow
+      text("2", 100, 100);
+      fill('grey');
+      ellipse(width / 2, height / 2 - 170, 150, 150);
+      fill('yellow');
+      ellipse(width / 2, height / 2, 150, 150);
+      fill('grey');
+      ellipse(width / 2, height / 2 + 170, 150, 150);
+      break;
+  }
+
+
+  timer = timer + 1;
+  if (timer > 100) {
+    timer = 0;
+    if (state > 2) state = 0;
+  }
 }
 
 
