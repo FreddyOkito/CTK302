f543408eeb1197e4bbf5a91f2c104be33512caf3
FreddyOkito <70274166+FreddyOkito@users.noreply.github.com>
2021-05-03 14:48:45 -0500

    .

--- Unit4/googledata/sketch.js
+++ Unit4/googledata/sketch.js
@@ -3,7 +3,9 @@ var bubbles = [];
 function setup() {
 
   // Tabletop stuff, for getting google spreadsheet data in.
-  let url = '1GtE3eoYVWBv9zMPoyettXzDCEv6qdNGKio_hgEh5duM'; // this is KEY of the URL from the sheet
+  //let url = '1GtE3eoYVWBv9zMPoyettXzDCEv6qdNGKio_hgEh5duM'; // this is KEY of the URL from the sheet
+let url = '1x2iodbIJi_PTDyXgIFrwErfJVhjHPV-_wRSL4wyhB5w'; // this is KEY of the URL from the sheet
+//https://docs.google.com/spreadsheets/d/1x2iodbIJi_PTDyXgIFrwErfJVhjHPV-_wRSL4wyhB5w/edit?usp=sharing
   let settings = {
     key: url, // The url of the published google sheet
     callback: gotData, // A callback for when the data comes in
@@ -30,7 +32,7 @@ function gotData(data) {
 
   // iterate through the array of data and create an object and push it on an array called bubbles
   for (let i = 0; i < data.length; i++) {
-    bubbles.push(new Bubble(data[i].Name, data[i].Shape)); // THESE Name and Shape need to match your column names in your spreadsheet!
+    bubbles.push(new Bubble(data[i].Juice, data[i].Name, data[i].Travel)); // THESE Name and Shape need to match your column names in your spreadsheet!
   }
 
 }
@@ -42,6 +44,7 @@ function draw() {
   // // iterate through the bubbles and display the objects!
   for (let i = 0; i < bubbles.length; i++) {
     bubbles[i].display();
+      bubbles[i].move();
   }
 
 }
@@ -50,22 +53,32 @@ function draw() {
 // my Bubble class
 class Bubble {
 
-  constructor(myName, myShape) {
-    this.name = myName;
-    this.shape = myShape;
+  constructor(myJuice, myName,myTravel) {
+    this.Juice = myJuice;
+    this.Name = myName;
+    this.Travel = myTravel;
     this.pos = createVector(random(width), random(height));
+    this.vel = createVector(random (5), 0);
 
   }
 
 
   display() {
-    if (this.shape == "Square") {
-      rect(this.pos.x, this.pos.y, 50, 50);
-    } else {
-      ellipse(this.pos.x, this.pos.y, 50, 50);
-    }
+    // if (this.shape == "Square") {
+    //   rect(this.pos.x, this.pos.y, 50, 50);
+    // } else {
+    //   ellipse(this.pos.x, this.pos.y, 50, 50);
+    // }
+    fill('yellow') ;
+    text(this.Name, this.pos.x, this.pos.y - 20);
+    text(this.Juice, this.pos.x, this.pos.y);
+    text(this.Travel, this.pos.x, this.pos.y + 20);
 
-    text(this.name, this.pos.x, this.pos.y);
+  }
+
+  move() {
+    this.pos.add(this.vel);
+    if(this.pos.x>width) this.pos.x=0;
   }
 
 
