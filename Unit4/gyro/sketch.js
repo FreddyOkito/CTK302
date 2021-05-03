088bb97e809ad2d08060d77aaf3814ab8d23d086
FreddyOkito <70274166+FreddyOkito@users.noreply.github.com>
2021-05-03 13:05:43 -0500

    .

--- Unit4/gyro/sketch.js
+++ Unit4/gyro/sketch.js
@@ -35,7 +35,7 @@ function draw() {
 
   rotate(radians(alpha)); // rotate the bunny depending on the alpha intake
 
-  image(bunnyImage, 0, 0, 500, 500);
+  image(bunnyImage, 0, 0, 750, 500);
   // rect(0, 0, 100, 100) ;
   pop();
 
